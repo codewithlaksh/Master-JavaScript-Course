baed2e7f1bc5466d99a283b6ece984a7ff92a32e
codewithlaksh <dashlakshyaraj2006@gmail.com>
2024-07-31 18:23:58 +0530

    Variables in JavaScript - Video 2

---
+++ js1.js
@@ -0,0 +1,58 @@
+// this is a single line comment
+
+/* this is a 
+multiline
+comment */
+
+// console.log("hello world")
+// console.error("hello world")
+// console.warn("hello world")
+// console.table({
+//     name: "Lakshyaraj",
+//     age: 18,
+//     marks: 96
+// })
+// console.assert(9 > 10)
+// console.assert(9 < 10)
+
+/* var userName = "Lakshyaraj";
+
+{
+    userName = "Rohan";
+    console.log(userName);
+}
+
+var userName = "Lakshyaraj"
+
+console.log(userName); */
+
+/*
+let userName = "Mayank";
+
+{
+    let userName = "Sahil";
+    console.log(userName);
+}
+
+// let userName = "Avisek"; // SyntaxError
+
+console.log(userName); */
+
+/*
+// const userName ; // SyntaxError
+const userName = "Mayank";
+// userName = "Sahil"; // TypeError
+console.log(userName); */
+
+/*
+----- Rules to Declare Variables -----
+1. Variables are case-sensitive;
+2. only underscores (_) and dollar ($) signs are allowed
+3. cannot contain numbers at the beginning
+4. can include numbers in between or at end of the variable
+*/
+
+let n1ame2 = "Lakshyaraj";
+let $name = "Lakshyaraj";
+let _name = "Lakshyaraj";
+
